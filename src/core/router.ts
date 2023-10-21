01d43ad44e5d26f265540d775df2b2c798533057
Arsalan Khattak <me@khattak.dev>
2023-10-21 12:57:25 +0500

    🐛 fix: / url should only match with /

--- src/core/router.ts
+++ src/core/router.ts
@@ -1,4 +1,9 @@
 export function matchRoute(pattern: string, url: string): boolean {
+  if ((pattern === "" || pattern === "/") && url === "/") {
+    return true;
+  } else if ((pattern === "" || pattern === "/") && url !== "/") {
+    return false;
+  }
   const regex = new RegExp(`${pattern}`);
   return regex.test(url);
 }
