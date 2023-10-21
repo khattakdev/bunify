f84f34fe02ef74ed7f2974db9acf7548c2effbd1
Arsalan Khattak <me@khattak.dev>
2023-10-21 12:57:53 +0500

    📦 misc: update dynamic routing example

--- examples/dynamic-routing/index.ts
+++ examples/dynamic-routing/index.ts
@@ -3,11 +3,16 @@ import App from "../../src/index";
 const app = new App();
 const port = 3001;
 
-app.get("/about", (_, res) => {
-  return res.send("Hello");
+app.get("ab(cd)?e", (_, res) => {
+  return res.send("ab(cd)?e");
 });
+
 app.get("ab?cd", (_, res) => {
   return res.send("ab?cd");
+});
+
+app.get("ab*cd", (_, res) => {
+  return res.send("ab*cd");
 });
 
 app.get("/", (_, res) => {
